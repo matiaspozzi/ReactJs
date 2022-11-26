34a531041cefabf1899f46b914fb333417017878
Matias <matias_age01@hotmail.com>
2022-11-25 21:41:06 -0300

    ruptura-de-app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React, { createContext } from "react"
+import React, { createContext, useState } from "react"
 import Header from "./components/header/Header"
 import Main from "./components/main/Main"
 import Footer from "./components/footer/Footer"
 import { BrowserRouter } from "react-router-dom"
 import ContextProvider from './components/ContextProvider';
+
 
 import { app, db } from "./firebase"
 
